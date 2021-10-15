8eec00e26677c4d7ff46e2e9a4598c784646b6b5
gnggng55 <kuk.kwge@gmail.com>
2021-10-15 13:07:51 +0900

    initial commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+
 
   def index
     @prototypes = Prototype.all
@@ -52,5 +54,9 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
